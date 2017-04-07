28f6e7a7315542b33ca669ce4e52b35ca9961aec
Frank.Zhang <xin.zhang@mevoco.com>
2017-04-07 19:59:24 -0400

    pysdk

--- zssdk.py
+++ zssdk.py
@@ -1,4 +1,8 @@
-
+import re
+import urllib3
+import string
+import json
+from uuid import uuid4
 
 CONFIG_HOSTNAME = 'hostname'
 CONFIG_PORT = 'port'
@@ -7,6 +11,16 @@ CONFIG_POLLING_INTERVAL = 'default_polling_interval'
 CONFIG_WEBHOOK = 'webhook'
 CONFIG_READ_TIMEOUT = 'read_timeout'
 CONFIG_WRITE_TIMEOUT = 'write_timeout'
+
+HEADER_JOB_UUID = "X-Job-UUID"
+HEADER_WEBHOOK = "X-Web-Hook"
+HEADER_JOB_SUCCESS = "X-Job-Success"
+HEADER_AUTHORIZATION = "Authorization"
+OAUTH = "OAuth"
+
+HTTP_ERROR = "sdk.1000"
+POLLING_TIMEOUT_ERROR = "sdk.1001"
+INTERNAL_ERROR = "sdk.1002"
 
 __config__ = {}
 
@@ -130,13 +144,100 @@ class AbstractAction(object):
                 value = str(value).strip()
                 setattr(self, param_name, value)
 
+    def _params(self):
+        ret = {}
+        for k, _ in self._param_descriptors:
+            val = getattr(self, k, None)
+            if val is not None:
+                ret[k] = val
 
+        return ret
+
+    def _url(self):
+        elements = ['http://', __config__[CONFIG_HOSTNAME], __config__[CONFIG_PORT]]
+
+        path = self.PATH.replace('{', '${')
+        unresolved = re.findall('${(.+?)}', path)
+        params = self._params()
+        if unresolved:
+            for u in unresolved:
+                if u in params:
+                    raise SdkError('missing a mandatory parameter[%s]' % u)
+
+        path = string.Template(path).substitute(params)
+        elements.append(path)
+
+        if self.HTTP_METHOD == 'GET' or self.HTTP_METHOD == 'DELETE':
+            elements.append('?')
+            elements.append('&'.join(['%s=%s' % (k, v) for k, v in params]))
+
+        return ''.join(elements), unresolved
+
+
+    def call(self, cb=None):
+        _error_if_not_configured()
+
+        self._check_params()
+        url, params_in_url = self._url()
+
+        headers = {}
+        if self.apiId is not None:
+            headers[HEADER_JOB_UUID] = self.apiId
+        else:
+            headers[HEADER_JOB_UUID] = _uuid()
+
+        web_hook = __config__.get(CONFIG_WEBHOOK, None)
+        if web_hook is not None:
+            headers[CONFIG_WEBHOOK] = web_hook
+
+        params = self._params()
+        body = None
+        if self.HTTP_METHOD == 'POST' or self.HTTP_METHOD == 'PUT':
+            m = {}
+            for k, v in params:
+                if v is None:
+                    continue
+
+                if k in params_in_url:
+                    continue
+
+                m[k] = v
+
+            body = {self.PARAM_NAME: m}
+
+        if not self.timeout:
+            self.timeout = __config__[CONFIG_READ_TIMEOUT]
+
+        rsp = _json_http(uri=url, body=body, headers=headers, method=self.HTTP_METHOD, timeout=self.timeout)
 
 
 class QueryAction(AbstractAction):
     def __init__(self):
         super(QueryAction, self).__init__()
 
+
+def _uuid():
+    return str(uuid4()).replace('-', '')
+
+
+def _json_http(
+        uri,
+        body=None,
+        headers={},
+        method='POST',
+        timeout=120.0
+):
+    pool = urllib3.PoolManager(timeout=timeout, retries=urllib3.util.retry.Retry(15))
+    headers.update({'Content-Type': 'application/json', 'Connection': 'close'})
+
+    if body is not None and not isinstance(body, str):
+        body = json.dumps(body).encode('utf-8')
+
+    if body:
+        headers['Content-Length'] = len(body)
+        return pool.request(method, uri, body=body, headers=headers)
+    else:
+        return pool.request(method, uri, headers=headers)
 
 class CreateZoneAction(AbstractAction):
 
